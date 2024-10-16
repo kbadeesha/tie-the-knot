f4e436bedce2706775c9af64afde7d6b4246b962
kbadeesha <adeesha.kristhorubaduge@gmail.com>
2024-10-16 11:40:04 +0530

    Password Strength Validator

--- src/app/components/TTKCustomTextField.tsx
+++ src/app/components/TTKCustomTextField.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { TextField, TextFieldProps } from "@mui/material";
+import {
+  TextField,
+  TextFieldProps,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
 import { SxProps } from "@mui/material";
+import { passwordStrengthValidation } from "../utils/passwordStrengthValidation";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
-interface TTKCustomTextFieldProps extends Omit<TextFieldProps, 'variant'> {
+interface TTKCustomTextFieldProps extends Omit<TextFieldProps, "variant"> {
   label: string;
   className?: string;
   sx?: SxProps;
@@ -14,14 +22,78 @@ const TTKCustomTextField: React.FC<TTKCustomTextFieldProps> = ({
   sx,
   ...otherProps
 }) => {
+  const [passwordValidation, setPasswordValidation] = React.useState({
+    strength: "",
+    color: "gray",
+  });
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    if (otherProps.type === "password") {
+      const validation = passwordStrengthValidation(value);
+      setPasswordValidation({
+        strength: validation.strength,
+        color: value ? validation.color : "gray",
+      });
+    }
+    if (otherProps.onChange) {
+      otherProps.onChange(event);
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
-    <TextField
-      label={label}
-      variant="outlined" // You can change this to "filled" or "standard" as needed
-      className={`border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
-      sx={sx}
-      {...otherProps}
-    />
+    <>
+      <TextField
+        label={label}
+        variant="outlined"
+        type={showPassword ? "text" : "password"} // Toggle password visibility
+        className={`border-gray-300 focus:outline-none ${className}`}
+        sx={{
+          ...sx,
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: passwordValidation.color,
+            },
+            "&:hover fieldset": {
+              borderColor: passwordValidation.color,
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: passwordValidation.color,
+            },
+          },
+        }}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={togglePasswordVisibility} edge="end">
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+        {...otherProps}
+        onChange={handleChange}
+      />
+      {otherProps.type === "password" && (
+        <span
+          style={{
+            marginTop: "8px",
+            display: "block",
+            color: passwordValidation.color,
+            fontSize: "12px",
+          }}
+        >
+          {otherProps.value != "" &&
+            passwordValidation.strength + " Password"}
+        </span>
+      )}
+    </>
   );
 };
 
---
+++ src/app/utils/passwordStrengthValidation.ts
@@ -0,0 +1,21 @@
+const atLeastMinimumLength = (password: string) => new RegExp(/(?=.{8,})/).test(password);
+const atLeastOneUppercaseLetter = (password: string) => new RegExp(/(?=.*?[A-Z])/).test(password);
+const atLeastOneLowercaseLetter = (password: string) => new RegExp(/(?=.*?[a-z])/).test(password);
+const atLeastOneNumber = (password: string) => new RegExp(/(?=.*?[0-9])/).test(password);
+const atLeastOneSpecialChar = (password: string) => new RegExp(/(?=.*?[#?!@$ %^&*-])/).test(password);
+
+export const passwordStrengthValidation = (password:any)=>{
+    if(!password)
+        if (!password) return { strength: 'Weak', color: 'red' };
+    let points =0;
+    if(atLeastMinimumLength(password)) points +=1 ;
+    if(atLeastOneUppercaseLetter(password)) points +=1 ;
+    if(atLeastOneLowercaseLetter(password)) points +=1 ;
+    if(atLeastOneNumber(password)) points +=1 ;
+    if(atLeastOneSpecialChar(password)) points +=1 ;
+    
+    if (points >= 5) return { strength: 'Strong', color: 'green' };
+    if (points >= 3) return { strength: 'Medium', color: 'yellow' };
+    return { strength: 'Weak', color: 'red' };
+
+}
