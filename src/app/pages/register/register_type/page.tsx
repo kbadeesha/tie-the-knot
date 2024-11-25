c2435a41562b8caf9891e7e85916653c14908e26
kbadeesha <adeesha.kristhorubaduge@gmail.com>
2024-11-25 15:23:48 +0530

    Signup method page UI

--- src/app/pages/register/register_type/page.tsx
+++ src/app/pages/register/register_type/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 // import { googleLogin } from "@/api/authApi";
 import TTKCustomButton from "@/app/components/common/TTKCustomButton";
+import { Box, Grid } from "@mui/material";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import loginImage from "../../../../../public/assets/images/cover5.jpg";
+import "../../../../styles/pages/register.css";
 
 const page = () => {
   // const handleGoogleLogin = async () => {
@@ -19,18 +23,57 @@ const page = () => {
   //   }
   // };
   return (
-    <div>
-      <Link
-        href="https://tietheknot-api.onrender.com/v1/auth/google-login?redirectUrl=http://localhost:3000/"
-        passHref
-      >
-        <TTKCustomButton type="button">
-          Signup using Google
-        </TTKCustomButton>
-      </Link>
-      <Link href="/pages/register" passHref>
-        <TTKCustomButton type="button">Signup using Email</TTKCustomButton>
-      </Link>
+    <div style={{ height: "100vh" }}>
+      <Grid container justifyContent="center" style={{ height: "100%" }}>
+        <Grid item xs={5} className="register-imageContainer">
+          <Image
+            src={loginImage}
+            alt="Login"
+            layout="fill"
+            objectFit="cover"
+            className="register-image"
+          />
+        </Grid>
+
+        <Grid item xs={7} className="register-formContainer">
+          <Box className="form-box" style={{ width: "65%" }}>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: "flex",
+                justifyContent: "center",
+                mb: 5,
+              }}
+            >
+              <Image
+                src={
+                  // "/assets/logo_lite.svg"
+                  "/assets/logo.svg"
+                }
+                alt="TieTheKnot"
+                width={120}
+                height={20}
+              />
+            </Box>
+            <Box className="button-container">
+              <Link
+                href="https://tietheknot-api.onrender.com/v1/auth/google-login?redirectUrl=http://localhost:3000/"
+                passHref
+              >
+                <TTKCustomButton type="button">
+                  Signup using Google
+                </TTKCustomButton>
+              </Link>
+              <span className="or-text">Or</span>
+              <Link href="/pages/register" passHref>
+                <TTKCustomButton type="button">
+                  Signup using Email
+                </TTKCustomButton>
+              </Link>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
     </div>
   );
 };
