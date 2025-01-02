26487c692572bba8c3673b6cb3bdf758b0777e94
kbadeesha <adeesha.kristhorubaduge@gmail.com>
2025-01-02 13:36:38 +0530

    filterData

--- src/app/data/filterData.ts
+++ src/app/data/filterData.ts
@@ -4,18 +4,22 @@
 
 const VENUE_EVENT_SPACES = [
     {
+        id:"indoor",
         value: "indoor",
         label: "Indoor"
     },
     {
+        id:"outdoor",
         value: "outdoor",
         label: "Outdoor"
     },
     {
+        id:"rooftop",
         value: "rooftop",
         label: "Rooftop"
     },
     {
+        id:"garden",
         value: "garden",
         label: "Garden"
     },
@@ -23,18 +27,22 @@ const VENUE_EVENT_SPACES = [
 
 const VENUE_CAPACITY = [
     {
+        id:"small",
         value: "small",
         label: "Small, Intimate Gathering (50 -100 guests)"
     },
     {
+        id:"medium",
         value: "medium",
         label: "Medium, Medium Wedding (100 - 250 guests)"
     },
     {
+        id:"large",
         value: "large",
         label: "Large, Larger Wedding (250 - 350 guests)"
     },
     {
+        id:"grand",
         value: "grand",
         label: "Grand, Grand Scale Event (350+ guests)"
     }
@@ -42,26 +50,32 @@ const VENUE_CAPACITY = [
 
 const VENUE_AMBIANCE = [
     {
+        id:"rustic",
         value: "rustic",
         label: "Rustic"
     },
     {
+        id:"modern",
         value: "modern",
         label: "Modern"
     },
     {
+        id:"classic",
         value: "classic",
         label: "Classic"
     },
     {
+        id:"industrial",
         value: "industrial",
         label: "Industrial"
     },
     {
+        id:"beachside",
         value: "beachside",
         label: "Beachside"
     },
     {
+        id:"mountain_forest_view",
         value: "mountain_forest_view",
         label: "Mountain/Forest View"
     }
@@ -69,34 +83,42 @@ const VENUE_AMBIANCE = [
 
 const VENUE_AMENITIES = [
     {
+        id:"catering",
         value: "catering",
         label: "Catering"
     },
     {
+        id:"buffet",
         value: "buffet",
         label: "Buffet"
     },
     {
+        id:"bar_services",
         value: "bar_services",
         label: "Bar Services"
     },
     {
+        id:"bridal_suite",
         value: "bridal_suite",
         label: "Bridal Suite"
     },
     {
+        id:"parking",
         value: "parking",
         label: "Parking"
     },
     {
+        id:"valet_service",
         value: "valet_service",
         label: "Valet Service"
     },
     {
+        id:"wheelchair_accessible",
         value: "wheelchair_accessible",
         label: "Wheelchair Accessible"
     },
     {
+        id:"pet_friendly",
         value: "pet_friendly",
         label: "Pet-Friendly"
     },
@@ -104,10 +126,12 @@ const VENUE_AMENITIES = [
 
  const VENUE_BAR_SERVICES=[
     {
+        id:"open_bar",
         value: "open_bar",
         label: "Open Bar"
     },
     {
+        id:"cash_bar",
         value: "cash_bar",
         label: "Cash Bar"
     },
@@ -115,60 +139,74 @@ const VENUE_AMENITIES = [
 
 const VENUE_COUPLES_STAY_PACKAGES =[
     {
+        id:"honeymoon_suite",
         value: "honeymoon_suite",
         label: "Honeymoon Suite"
     },
     {
+        id:"complimentary_night_for_couple",
         value: "complimentary_night_for_couple",
         label: "Complimentary Night for the Couple"
     },
     {
+        id:"early_checkin_late_checkout",
         value: "early_checkin_late_checkout",
         label: "Early Check-In and Late Check-Out Options"
     },
     {
+        id:"in_room_breakfast_champagne",
         value: "in_room_breakfast_champagne",
         label: "In-room Breakfast/Champagne Service"
     },
     {
+        id:"post_wedding_brunch",
         value: "post_wedding_brunch",
         label: "Post-wedding Brunch or Breakfast Option"
     },
     {
+        id:"discounted_rates_for_family",
         value: "discounted_rates_for_family",
         label: "Discounted Rates for Family"
     },
 ]
 const VENUE_ADDITIONAL_EQUIPMENT = [
     {
+        id:"generators",
         value: "generators",
         label: "Generators"
     },
     {
+        id:"climate_control_cooling",
         value: "climate_control_cooling",
         label: "Climate Control - Cooling"
     },
     {
+        id:"tents_canopies",
         value: "tents_canopies",
         label: "Tents and Canopies"
     },
     {
+        id:"stage_dance_floor",
         value: "stage_dance_floor",
         label: "Stage and Dance Floor"
     },
     {
+        id:"projectors",
         value: "projectors",
         label: "Projectors"
     },
     {
+        id:"microphones",
         value: "microphones",
         label: "Microphones"
     },
     {
+        id:"cake_table",
         value: "cake_table",
         label: "Cake Table"
     },
     {
+        id:"gift_table",
         value: "gift_table",
         label: "Gift Table"
     }
